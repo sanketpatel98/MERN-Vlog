c7ddb16acd410bd121278ce97678bc3673c2936e
sanketpatel <patelsanketr98@gmail.com>
2024-02-13 19:59:44 -0500

    completed user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -4,32 +4,34 @@ import { HiUser, HiArrowSmRight } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 
 export default function DashSidebar() {
-    const location = useLocation();
-    const [tab, setTab] = useState("");
-    useEffect(() => {
-      const urlParamas = new URLSearchParams(location.search);
-      const tabFromUrl = urlParamas.get("tab");
-      if (tabFromUrl) {
-        setTab(tabFromUrl);
-      }
-    }, [location.search]);
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParamas = new URLSearchParams(location.search);
+    const tabFromUrl = urlParamas.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark">
-            Profile
-          </Sidebar.Item>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
           </Link>
-        <Sidebar.Item
-          icon={HiArrowSmRight}
-          className='cursor-pointer'
-        >
-          Sign Out
-        </Sidebar.Item>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
         </Sidebar.ItemGroup>
-
       </Sidebar.Items>
     </Sidebar>
   );
