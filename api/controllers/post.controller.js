e1962d5c8462c1c0bc0bf9b4e6cdfc773d915d4b
sanketpatel <patelsanketr98@gmail.com>
2024-02-17 12:15:32 -0500

    created getposts API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,21 +1,88 @@
 import Post from "../models/post.model.js";
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-    if (!req.user.isAdmin) {
-        return next(errorHandler(403, 'You are not allowed to create a post'))
-    }
-    if (!req.body.title || !req.body.content) {
-        return next(errorHandler(400, 'Please provide all required fields'))
-    }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g,'');
-    const newPost = new Post({
-        ...req.body, slug, userId: req.user.id
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required fields"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
     })
-    try {
-        const savedPost = await newPost.save();
-        res.status(201).json(savedPost);
-    } catch (error) {
-        next(error);
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const lastMonthPosts = await Post.countDocuments({
+        createdAt: { $gte: getOneMonthAgoDate() },
+    })
+
+    res.status(200).json({
+        posts,
+        totalPosts,
+        lastMonthPosts
+    })
+  } catch (error) {
+    next(error);
+  }
+};
+
+function getOneMonthAgoDate() {
+    const today = new Date();
+    const currentMonth = today.getMonth(); 
+  
+    let monthAgoMonth = currentMonth - 1;
+    let monthAgoYear = today.getFullYear();
+  
+    if (monthAgoMonth < 0) {
+      monthAgoMonth = 11; 
+      monthAgoYear--;
     }
-}
+  
+    const monthAgoDate = new Date(monthAgoYear, monthAgoMonth, 1);
+  
+    if (today.getDate() === new Date(today.getFullYear(), today.getMonth(), 0).getDate()) {
+      monthAgoDate.setDate(Math.min(today.getDate(), new Date(monthAgoYear, monthAgoMonth, 0).getDate()));
+    }
+  
+    return monthAgoDate;
+  }
+  
+  
