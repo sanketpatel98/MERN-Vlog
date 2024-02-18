d7afec01f6b177bd56fc1214464b51faf6675d91
sanketpatel <patelsanketr98@gmail.com>
2024-02-18 09:49:31 -0500

    added delete user functionality to the admin dashboard

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -52,7 +52,20 @@ export default function DashUsers() {
     }
   };
 const handleDeleteUser = async () => {
-
+  try {
+    const res = await fetch(`/api/user/delete/${userIdToDelete}`,{
+      method: 'DELETE'
+    });
+    const data = await res.json();
+    if (res.ok) {
+      setUser((prev)=>prev.filter((user)=>user._id !== userIdToDelete))
+      setShowModal(false);
+    } else {
+      console.log(data.message);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 }
   return (
     <div className="table-auto overflow-x-auto md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
