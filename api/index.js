ee1bb4f1c8447c2e06792fbf4b8dc05bcc3a75e9
sanketpatel <patelsanketr98@gmail.com>
2024-02-19 18:07:32 -0500

    added pre installation

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-import postRoutes from './routes/post.route.js'
-import commentRoute from './routes/comment.route.js'
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoute from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -17,6 +18,8 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -27,16 +30,22 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoute);
 
-app.use((err, req, res, next) =>{
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error!'
+  const message = err.message || "Internal Server Error!";
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   });
 });
